c19cd520529fac1cd7c4cd8a4ae9a4ff42257f60
davidenovarini <116503113+davidenovarini@users.noreply.github.com>
2022-12-11 12:40:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
+#include <iostream>
+using namespace std;
 
+float maxarray (int a) {
+    float numeri [a];
+    float b= numeri [0];
+    for (int k=0; k<a ; k++)
+        numeri [k]=0;
+    for (int i=0; i<a ; i++ ) {
+         cin >> numeri [i];
+    }
+    for (int j = 0; j < a; j++) {
+        if (b < numeri[j])
+            b = numeri[j];
+    }
+    return b;
+}
